1ef1103511227fc3b058851cf2daffeb41ce5d83
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-04-14 12:08:48 +0530

    first

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./modules/layout/navbar/navbar";
+let App = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+};
+export default App;

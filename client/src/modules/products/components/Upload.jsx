c88fdd12b2335112f9ea5db97f9cd5fe4cb92130
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-04-16 12:08:11 +0530

    git push

--- client/src/modules/products/components/Upload.jsx
+++ client/src/modules/products/components/Upload.jsx
@@ -1,8 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 let Upload = () => {
+  let [product, setProduct] = useState({
+    productName: "",
+    price: "",
+    qty: "",
+    file: "",
+    avail: "",
+    desc: "",
+    usage: "",
+  });
+  let inputHandler = (event) => {
+    setProduct({ ...product, [event.target.name]: event.target.value });
+  };
   return (
     <>
-      <h1> Upload Products</h1>
+      <section></section>
+      <section>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-8">
+            <pre></pre>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Product Name"
+                    onChange={inputHandler}
+                    name="productName"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Price"
+                    onChange={inputHandler}
+                    name="price"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="QTY"
+                    onChange={inputHandler}
+                    name="qty"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="file"
+                    className="form-control"
+                    placeholder="Please upload image"
+                    onChange={inputHandler}
+                    name="image"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Available"
+                    onChange={inputHandler}
+                    name="avail"
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    placeholder="Product Description"
+                    className="form-control"
+                    onChange={inputHandler}
+                    name="desc"
+                  ></textarea>
+                </div>
+                <div className="form-group">
+                  <textarea
+                    placeholder="Product Description"
+                    className="form-control"
+                    onChange={inputHandler}
+                    name="usage"
+                  ></textarea>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="submit"
+                    className="form-control btn btn-primary"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- client/src/modules/user/components/Signup.jsx
+++ client/src/modules/user/components/Signup.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 let Signup = () => {
+  let [user, setUser] = useState({ user: "", email: "", password: "" });
+  let inputHandler = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  let submitHandler = (event) => {
+    console.log(user)
+    event.preventDefault();
+  };
   return (
     <React.Fragment>
-      <h2> Signup Componet</h2>
+      <section></section>
+      <section>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">
+                  <p className="display-6">Registration Details</p>
+                  <pre>{JSON.stringify(user)}</pre>
+                </div>
+                <div className="card-body">
+                  <form onSubmit={submitHandler}>
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="user"
+                        onChange={inputHandler}
+                        placeholder="User Name"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="email"
+                        onChange={inputHandler}
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="password"
+                        onChange={inputHandler}
+                        placeholder="Password"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        type="submit"
+                        className="form-control btn btn-success"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
 };
